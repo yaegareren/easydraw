312cab10a1a3cef80d98e89a58cf93ee6669d724
Shree Mangalam <sshreemangalam@gmail.com>
2023-12-24 23:47:23 +0530

    provide backend endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://easydraw-server.onrender.com"
+    : "http://localhost:5000";
 export const socket = io("http://localhost:5000");
